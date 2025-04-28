786bdd16758f6716ff0ea4b561f98b18b6f25cf4
Dave Liefbroer <dave@liefbroer.com>
2025-04-28 13:14:04 +0200

    Too big

--- keyboards/sofle_choc/keymaps/miryoku/keymap.c
+++ keyboards/sofle_choc/keymaps/miryoku/keymap.c
@@ -43,22 +43,6 @@
 
 #pragma once
 
-#define XXX KC_NO
-
-#define LAYOUT_miryoku(\
-    K00, K01, K02, K03, K04,                K05, K06, K07, K08, K09,\
-    K10, K11, K12, K13, K14,                K15, K16, K17, K18, K19,\
-    K20, K21, K22, K23, K24,                K25, K26, K27, K28, K29,\
-    N30, N31, K32, K33, K34,                K35, K36, K37, N38, N39\
-)\
-LAYOUT(\
-XXX, XXX, XXX, XXX, XXX, XXX,                XXX, XXX, XXX, XXX, XXX, XXX,\
-XXX, K00, K01, K02, K03, K04,                K05, K06, K07, K08, K09, XXX,\
-XXX, K10, K11, K12, K13, K14,                K15, K16, K17, K18, K19, XXX,\
-XXX, K20, K21, K22, K23, K24, XXX,      XXX, K25, K26, K27, K28, K29, XXX,\
-         XXX, XXX, K32, K33, K34,      K35, K36, K37, XXX, XXX\
-)
-
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_MPRV, KC_MNXT) },
